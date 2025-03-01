f66e138b58a6921cf352d7e571e384b69434d126
himiyosh <himiyosh@gmail.com>
2025-03-01 19:41:40 +0900

    Add files

--- Conv2pcapng.psm1
+++ Conv2pcapng.psm1
@@ -1,5 +1,5 @@
 function Conv2pcapng($orgfile){
 	$fileName = [System.IO.Path]::GetFileNameWithoutExtension($orgfile)
 	$outfile = $fileName + ".pcapng"
-	C:\Tools\etl2pcapng\etl2pcapng.exe $orgfile $outfile
+	etl2pcapng.exe $orgfile $outfile
 }
